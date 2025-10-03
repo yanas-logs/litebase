1b08a2e8d0d72af868c511ea5cc2393f49343229
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-03 15:23:07 +0000

    Remove inline styles from Header component for cleaner markup

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header style={{ width: "100%", margin: 0, padding: 0 }}>
+    <header className="">
       <Navbar />
     </header>
   );
