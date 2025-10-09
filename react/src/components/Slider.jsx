74fdd6b686abc67c22cfcd057f431cdbfb1ed10e
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-09 10:44:10 +0000

    Add autoplay functionality to Slider component using Swiper

--- react/src/components/Slider.jsx
+++ react/src/components/Slider.jsx
@@ -3,18 +3,22 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "../components/css/Slider.css";
-import { Navigation, Pagination, A11y } from "swiper/modules";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules";
 import images from "../data/images.json";
 
 export const Slider = ({ slides = images }) => {
   return (
     <Swiper
-      modules={[Navigation, Pagination, A11y]}
+      modules={[Navigation, Pagination, A11y, Autoplay]}
       spaceBetween={30}
       slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
       loop={true}
+      autoplay={{
+          delay: 5000,
+          disableOnInteraction: false, 
+        }}
     >
       {slides.map((slide) => (
         <SwiperSlide key={slide.id}>
