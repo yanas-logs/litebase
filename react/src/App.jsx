f2741b566c48d17c5f0ca055c01a208de87c121b
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-09 14:54:57 +0000

    Refactor App.jsx and add Home.jsx component with Slider.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Slider } from './components/Slider';
+import Home from "./pages/Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,20 +10,9 @@ function App() {
   return (
     <>
       <Header />
-      
-
-      <main style={{ padding: "1px" }}>
-        <Slider />
+      <main>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <h1>Welcome 🚀</h1>
-                <p>Main content area.</p>
-              </>
-            }
-          />
+          <Route path="/" element={<Home />} />
           <Route path="/product" element={<h1>Product Page</h1>} />
           <Route path="/about" element={<h1>About Page</h1>} />
           <Route path="/contact" element={<h1>Contact Page</h1>} />
---
+++ react/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Slider } from "../components/Slider";
+
+export default function Home() {
+  return (
+    <div>
+      <Slider />
+      <h1>Welcome 🚀</h1>
+      <p>Main content area.</p>
+    </div>
+  );
+}
