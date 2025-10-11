1fff94e382a8a04d2c5ef7843dfa4c1044775a6c
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-11 16:53:04 +0000

    Adding logo to footer

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
+import brand_logo from "../assets/brand_logo.svg";
 import "./css/Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
+      <div className="footer-container">
 
-        <div className="footer-logo">Footer Page</div>
-        <ul className="footer-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/privacy">Privacy</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
+        {/* Left: Logo */}
+        <div className="footer-left">
+          <Link to="/" className="footer-logo">
+            <img src={brand_logo} alt="logo" className="footer-logo-img" />
+            <span className="footer-brand">.</span>
+          </Link>
+        </div>
 
+        {/* Center: Navigation */}
+        <div className="footer-center">
+          <ul className="footer-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/product">Product</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/privacy">Privacy</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Copyright: Center Bottom */}
+      <div className="footer-bottom">
         <p className="footer-copy">
           © {new Date().getFullYear()} yanas-logs. All rights reserved.
         </p>
