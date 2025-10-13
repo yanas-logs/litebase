e5e1a0b38d52313e2ba5cf01f589583738c5dd68
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-13 16:57:34 +0000

    add page (app) and component page (Home)

---
+++ nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from './components/Home/page'
+
+export default function Page() {
+    return (
+        <div>
+            <Home />
+        </div>
+    )
+}
