50c458fe6a9664e46b09cf667bee1f09fff9ad98
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-18 15:56:14 +0000

    route link About & Contact page from Navbar menu

--- nextjs/src/app/components/layout/Navbar.tsx
+++ nextjs/src/app/components/layout/Navbar.tsx
@@ -13,10 +13,10 @@ export default function Navbar() {
           <Link href="/" className="hover:text-gray-300">Home</Link>
         </li>
         <li>
-          <Link href="/about" className="hover:text-gray-300">About</Link>
+          <Link href="/components/About" className="hover:text-gray-300">About</Link>
         </li>
         <li>
-          <Link href="/contact" className="hover:text-gray-300">Contact</Link>
+          <Link href="/components/Contact" className="hover:text-gray-300">Contact</Link>
         </li>
       </ul>
     </nav>
