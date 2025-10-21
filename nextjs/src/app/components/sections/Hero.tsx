b41c8dbf14098692582d175ce5fafbbbd9a671eb
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-21 15:53:04 +0000

    chore ( style ): set Hero section background.

--- nextjs/src/app/components/sections/Hero.tsx
+++ nextjs/src/app/components/sections/Hero.tsx
@@ -5,18 +5,25 @@ import Button from "../ui/Button";
 
 export default function Hero() {
   return (
-    <section className="bg-gray-100 py-25">
-        <div className="container mx-auto px-4 text-center"> 
-            <h1 className="text-4xl md:text-5xl font-bold mb-6">
-                Welcome to Our Next.js App
-            </h1>
-            <p className="text-lg md:text-xl mb-8 text-gray-700">
-                Building modern web applications with ease and efficiency.
-            </p>
-            <Button variant="primary" size="lg">
-                Get Started
-            </Button>
-        </div>
+    <section
+      className="relative h-screen bg-cover bg-center flex items-center justify-center text-center"
+      style={{ backgroundImage: "url('/image-cozy-library.webp')" }}
+    >
+      {/* Overlay more darker for readable */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      {/* Main Content */}
+      <div className="relative z-10 text-white px-4">
+        <h1 className="text-4xl md:text-5xl font-bold mb-6">
+          Welcome to Our Next.js App
+        </h1>
+        <p className="text-lg md:text-xl mb-8 text-gray-200">
+          Building modern web applications with ease and efficiency.
+        </p>
+        <Button variant="primary" size="lg">
+          Get Started
+        </Button>
+      </div>
     </section>
   );
 }
