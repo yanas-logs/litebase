42472eaea2738c34a2d9d800cb5405601734b38c
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-22 16:25:36 +0000

    add route Product to Footer

--- nextjs/src/app/components/layout/Footer.tsx
+++ nextjs/src/app/components/layout/Footer.tsx
@@ -12,6 +12,7 @@ export default function Footer() {
         <nav className="mt-2">
           <Link href="/" className="mx-2 hover:underline">Home</Link>
           <Link href="/components/About" className="mx-2 hover:underline">About</Link>
+          <Link href="/components/Product" className="mx-2 hover:underline">Product</Link>
           <Link href="/components/Contact" className="mx-2 hover:underline">Contact</Link>
         </nav>
       </div>
