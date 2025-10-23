5aee2ad0ffc4e3f5c1a41112d7760b1d4ffd9cb4
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-23 11:03:19 +0000

    chore: refactor margin top from footer

--- nextjs/src/app/components/layout/Footer.tsx
+++ nextjs/src/app/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return(
-    <footer className="bg-gray-900 text-white p-4 mt-8">
+    <footer className="bg-gray-900 text-white p-4">
       <div className="container mx-auto text-center">
         <p>&copy; {year} My Next.js App. All rights reserved.</p>
         <nav className="mt-2">
