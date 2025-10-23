d948a343fe42df023d2ca5a8ed36274939d85f76
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-10-23 11:00:33 +0000

    refactor: screen height layout

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex items-center justify-center min-h-screen p-6 bg-gray-50">{children}</main>
+        <main className="flex items-center justify-center bg-gray-50">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
