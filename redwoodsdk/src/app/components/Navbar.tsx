37c87f140a8994f24052f69feb5fcfabf180dbb2
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-02 16:44:16 +0000

    Create Navbar components to redwoodsdk

---
+++ redwoodsdk/src/app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>RedwoodSDK</div>
+            <ul className={styles.navLinks}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Docs</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
