aa1452c5ffe18cf784c239f5e1603c72ec77ceaa
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-03 16:34:46 +0000

    chore: update Navbar links for Home and About pages

--- redwoodsdk/src/app/components/Navbar.tsx
+++ redwoodsdk/src/app/components/Navbar.tsx
@@ -6,9 +6,9 @@ export const Navbar = () => {
         <nav className={styles.navbar}>
             <div className={styles.logo}>RedwoodSDK</div>
             <ul className={styles.navLinks}>
-                <li><a href="#">Home</a></li>
+                <li><a href="/">Home</a></li>
                 <li><a href="#">Docs</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="/about">About</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
