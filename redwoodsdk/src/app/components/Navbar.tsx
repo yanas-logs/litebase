8c1312c4f9cee3ad6e7f088d5b66263d495a3f55
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-04 16:58:07 +0000

    chore: update  Navbar links for Contact page

--- redwoodsdk/src/app/components/Navbar.tsx
+++ redwoodsdk/src/app/components/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
         <li><a href="/">Home</a></li>
         <li><a href="#">Docs</a></li>
         <li><a href="/about">About</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
